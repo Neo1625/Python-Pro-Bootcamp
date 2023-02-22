64a34665e004d21e96546a48f468529fa45761ed
neotron1625@gmail.com <86784170+snare0625@users.noreply.github.com>
2023-02-22 20:35:10 +0200

    Searching or cheap flights

--- Python Pro Bootcamp/Flight Deal Finder/flight_data.py
+++ Python Pro Bootcamp/Flight Deal Finder/flight_data.py
@@ -1,3 +1,37 @@
+#This class is responsible for structuring the flight data.
+import requests
+import datetime
+
+FLIGHT_SEARCH_ENDPOINT = "https://api.tequila.kiwi.com/v2/search"
+API_KEY = "c_Dc_td_4P0St3Q4p53YvvMrZk0nRznR"
+
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+    def __init__(self, destination_city):
+        self.destination_city = destination_city
+
+    def flight_price(self):
+        header = {"apikey": API_KEY}
+        payload = {
+            "fly_from": "LON",
+            "fly_to": self.destination_city,
+            "date_from": self.date_from_to_calc(1),
+            "date_to": self.date_from_to_calc(6*30),
+            "one_for_city": 1,
+            "flight_type": "round",
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "curr": "GBP"
+        }
+        response = requests.get(url=FLIGHT_SEARCH_ENDPOINT, headers=header, params=payload)
+        price = response.json()['data'][0]['price']
+        return price
+
+
+
+
+
+    def date_from_to_calc(self, num_of_days):
+        now = datetime.datetime.now()
+        tomorrow = now + datetime.timedelta(days=num_of_days)
+        return tomorrow.strftime("%d/%m/%Y")
+
--- Python Pro Bootcamp/Flight Deal Finder/main.py
+++ Python Pro Bootcamp/Flight Deal Finder/main.py
@@ -1,6 +1,8 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from flight_search import FlightSearch
 from data_manager import DataManager
+from flight_data import FlightData
+import datetime
 from pprint import pprint
 
 data_manager = DataManager()
@@ -12,7 +14,11 @@ for flight in sheet_data:
 
 data_manager.iataCode_sheet_update(sheet_data)
 
-pprint(sheet_data)
+for city in sheet_data:
+    destination = city['iataCode']
+    flight_data = FlightData(destination)
+    print(f"{destination}: {flight_data.flight_price()}")
+
 
 
 
